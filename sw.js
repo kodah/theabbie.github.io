25d122b28f2c125720363a5027b51f82c7ae627b
abhishek <abhishek7gg7@gmail.com>
2019-11-12 21:36:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('fetch', function(event) {                                                     2
+self.addEventListener('fetch', function(event) {
     event.respondWith(
     new Response('<p>This is a response that comes from your service worker!</p>', {
        headers: { 'Content-Type': 'text/html' }                        
