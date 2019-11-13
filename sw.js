dc58e684936f70c2df0ffbcb2077e9591e88fd3e
abhishek <abhishek7gg7@gmail.com>
2019-11-13 12:47:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 self.addEventListener('fetch', async function(event) {
-event.respondWith(new Response(JSON.stringify(event)),{headers: {'Content-Type': 'text/html'}});
+event.respondWith(new Response(JSON.stringify(event.request)),{headers: {'Content-Type': 'text/html'}});
 });
