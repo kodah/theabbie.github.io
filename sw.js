5ed6a6a604e2af1864e99ebea98d39f47e9fb97d
abhishek <abhishek7gg7@gmail.com>
2019-11-13 19:38:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('fetch', async function(event) {
 if (event.request.url.startsWith("https://theabbie.github.io")) {
-event.respondWith(fetch("https://api.github.com/repos/theabbie/theabbie.github.io/contents/"+event.request.url.substring(26)).then(x=>x.json()).then(function(code) {return (new Response(atob(code.content),{headers: {"Content-Type": "text/html"}}))}).catch(function(err) {return (new Response("<h1>404</h1>",{headers: {"Content-Type": "text/html"}}))}));
+event.respondWith(fetch("https://api.github.com/repos/theabbie/theabbie.github.io/contents/"+event.request.url.substring(26)).then(x=>x.json()).then(function(code) {return (new Response("#theabbie"+atob(code.content),{headers: {"Content-Type": "text/html"}}))}).catch(function(err) {return (new Response("#theabbie"+"<h1>404</h1>",{headers: {"Content-Type": "text/html"}}))}));
 }
 });
 
