f15782943b0ddacceae052ae157ddee0e2d0039c
abhishek <abhishek7gg7@gmail.com>
2019-11-13 13:38:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
 self.addEventListener('fetch', async function(event) {
-event.respondWith(new Response(JSON.stringify(event.request)),{headers: {'Content-Type': 'text/html'}});
+if (event.request.url="https://theabbie.github.io/pwa") {
+event.respondWith(new Response("okay"))
+}
 });
