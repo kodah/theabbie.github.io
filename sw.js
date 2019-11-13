eb9715b213c4c1a26da1391db7f189d234ac9d73
abhishek <abhishek7gg7@gmail.com>
2019-11-13 12:23:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-self.addEventListener('fetch', function(event) {
-fetch("https://api.github.com/repos/theabbie/theabbie.github.io/contents"+event.request.url.substring(26)).then(x=>x.json()).then(function(code) {
-event.respondWith(new Response(code.content));
-})
+self.addEventListener('fetch', async function(event) {
+event.respondWith((await (await fetch("https://api.github.com/repos/theabbie/theabbie.github.io/contents"+event.request.url.substring(26))).json()).content);
 });
