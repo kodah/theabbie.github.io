a2930e1324da6a7ab91ab3b05b5a714b94d447fe
abhishek <abhishek7gg7@gmail.com>
2019-11-13 11:47:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,3 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(self.clients.claim());
-});
-
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-    new Response('<p>This is a response that comes from your service worker!</p>', {
-       headers: { 'Content-Type': 'text/html' }                        
-      });
-    );
+event.respondWith(new Response('Fetch handler for ' + event.request.url));
 });
