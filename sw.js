46ccb95506b9831655e7081da6190ea9b31af16c
abhishek <abhishek7gg7@gmail.com>
2019-11-14 06:49:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
+self.addEventListener('install', async function(event) {
+event.waitUntil(
+    caches.open("TestCacheV1").then(function(cache) {
+      cache.add("/");
+    })
+  );
+});
+
 self.addEventListener('fetch', async function(event) {
-if (event.request.url=="https://theabbie.github.io/") {
-event.respondWith(fetch("https://typi.tk/?url=https://m.wikihow-fun.com/Special:Randomizer&sel=.step&attribs=classs&static=true&join=%3Cbr%3E"));
-}
+event.respondWith(
+caches.match(event.request).then(function(response) {
+return response || fetch(event.request);
+})
+);
 });
 
 //new Response("<meta http-equiv='refresh' content='0;url=https://theabbie.github.io/' /><meta name='viewport' content='width=device-width, initial-scale=1.0'><h1>"+Math.random().toString()+"</h1>",{headers: {"Content-Type": "text/html"}})
