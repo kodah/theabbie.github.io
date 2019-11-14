88b064aec74b2580ad89b4879ced5ca00b5364a1
abhishek <abhishek7gg7@gmail.com>
2019-11-14 06:52:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ event.waitUntil(
 });
 
 self.addEventListener('fetch', async function(event) {
+console.log(event.request.url);
 event.respondWith(
 caches.match(event.request).then(function(response) {
 return response || fetch(event.request);
