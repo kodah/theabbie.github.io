b655808dc00563acdab300a3e8677de3556875f0
abhishek <abhishek7gg7@gmail.com>
2019-11-14 07:19:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,9 @@
-self.addEventListener('install', async function(event) {
-event.waitUntil(
-    caches.open("TestCacheV1").then(function(cache) {
-      cache.add("/");
-    })
-  );
-});
-
 self.addEventListener('fetch', async function(event) {
-console.log(event.request.url);
 event.respondWith(
-caches.match(event.request).then(function(response) {
-return response || fetch(event.request).then(function(res) {
-if (event.request.url.startsWith("https://kit")) {cache.put(event.request,res.clone())}
-return res;
-});
+if (event.request.url.startsWith("https://theabbie.github.io/html")) {
+fetch("https://thedb.now.sh/data").then(x=>x.text()).then(function(code) {
+return (new Response(code,{headers: {"Content-Type": "text/html"}}))
 })
+}
 );
 });
-
-//new Response("<meta http-equiv='refresh' content='0;url=https://theabbie.github.io/' /><meta name='viewport' content='width=device-width, initial-scale=1.0'><h1>"+Math.random().toString()+"</h1>",{headers: {"Content-Type": "text/html"}})
