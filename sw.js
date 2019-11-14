cdad6ed58faef0016d9532b92712c2638132f32a
abhishek <abhishek7gg7@gmail.com>
2019-11-14 07:21:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 self.addEventListener('fetch', async function(event) {
-event.respondWith(
 if (event.request.url.startsWith("https://theabbie.github.io/html")) {
+event.respondWith(
 fetch("https://thedb.now.sh/data").then(x=>x.text()).then(function(code) {
 return (new Response(code,{headers: {"Content-Type": "text/html"}}))
 })
-}
 );
+}
 });
