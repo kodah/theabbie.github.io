afbb7e89d07591419dde1645b0dcffa6d2942337
abhishek <abhishek7gg7@gmail.com>
2019-11-14 05:59:28 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 self.addEventListener('fetch', async function(event) {
 if (event.request.url=="https://theabbie.github.io/") {
-event.respondWith(new Response("<meta http-equiv='refresh' content='0;url=https://theabbie.github.io/' /><meta name='viewport' content='width=device-width, initial-scale=1.0'><h1>"+Math.random().toString()+"</h1>",{headers: {"Content-Type": "text/html"}}));
+event.respondWith(fetch("https://google.com"));
 }
 });
+
+//new Response("<meta http-equiv='refresh' content='0;url=https://theabbie.github.io/' /><meta name='viewport' content='width=device-width, initial-scale=1.0'><h1>"+Math.random().toString()+"</h1>",{headers: {"Content-Type": "text/html"}})
