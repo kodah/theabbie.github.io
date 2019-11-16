1460e81e015c7e1359441e14a3c3932f64137d50
abhishek <abhishek7gg7@gmail.com>
2019-11-17 00:06:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener('fetch', async function(event) {
 if (event.request.url.startsWith("https://theabbie.github.io/html")) {
 event.respondWith(
-fetch("https://thedb.now.sh/data").then(x=>x.text()).then(function(code) {
-return (new Response(code,{headers: {"Content-Type": "text/html"}}))
+fetch("https://codrcrew.firebaseio.com/main/html.json").then(x=>x.text()).then(function(code) {
+return (new Response(unescape(code.slice(1,-1)),{headers: {"Content-Type": "text/html"}}))
 })
 );
 }
