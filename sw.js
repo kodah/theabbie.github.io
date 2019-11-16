c4023f6dcbce88695e67d12af1a8ecb9d5fb8d00
abhishek <abhishek7gg7@gmail.com>
2019-11-17 01:25:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
 self.addEventListener('fetch', async function(event) {
 if (event.request.url.startsWith("https://theabbie.github.io/html")) {
+event.waitUntil(
+self.registration.showNotification("title", {  
+body: "body",  
+icon: "blog/72fe95c5576ec634e214814a32ab785568eda76a.png",  
+tag: "tag"  
+})  
+);  
 event.respondWith(
 fetch("https://codrcrew.firebaseio.com/main/html.json").then(x=>x.text()).then(function(code) {
 return (new Response(atob(code.slice(1,-1)),{headers: {"Content-Type": "text/html"}}))
