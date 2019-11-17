5b44bcd2c47331c82873996654131abb931a6ba1
abhishek <abhishek7gg7@gmail.com>
2019-11-17 20:33:28 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,35 @@
 self.addEventListener('fetch', async function(event) {
 if (event.request.url.startsWith("https://theabbie.github.io/html")) {
-event.waitUntil(
-self.registration.showNotification("title", {  
-body: "body",  
-icon: "blog/72fe95c5576ec634e214814a32ab785568eda76a.png",  
-tag: "tag"  
-})  
 );  
 event.respondWith(
 fetch("https://codrcrew.firebaseio.com/main/html.json").then(x=>x.text()).then(function(code) {
 return (new Response(atob(code.slice(1,-1)),{headers: {"Content-Type": "text/html"}}))
 })
 );
+}
+else if (!navigator.onLine) {
+event.respondWith(new Response(`<html>
+<head>
+<title>The Abbie</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+ @font-face {
+	font-family: kirvy;
+        font-display: swap;
+	src: url('https://cdn.jsdelivr.net/gh/theabbie/example@gh-pages/files/kirvy.otf');
+}
+html {scroll-behavior: smooth;}
+* {font-size: 15px; letter-spacing: 6px; word-spacing: 12px; line-height: 125%;}
+body {margin: 0 0 0 0; background-color: rgb(248,248,248); color: black; font-family: kirvy;}
+.header {display: block; width: 100%; text-align: center; padding: 20 0 20 0;font-weight: bolder;font-size: 30px; background-color: rgb(72,113,197); color: white;}
+</style>
+</head>
+<body>
+<div class="header">@TheAbbie</div>
+<center>
+<h1>You Are Offline !</h1>
+</center>
+</body>
+</html>`,{headers: {"Content-Type": "text/html"}}))
 }
 });
