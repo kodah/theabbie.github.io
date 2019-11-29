cfa5f5384d4a7e4bc8403dd9b3425ec2555b025b
abhishek <abhishek7gg7@gmail.com>
2019-11-29 18:36:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,48 +1,9 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'images',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-    ],
-  })
+  /.*/,
+  new workbox.strategies.StaleWhileRevalidate(),
 );
-
-workbox.routing.registerRoute(
-  /\.(?:js|css)$/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'static-resources',
-  })
-);
-
-workbox.routing.registerRoute(
-  ({event}) => event.request.destination === 'font',
-  new workbox.strategies.CacheFirst({
-    cacheName: 'fonts',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-    ],
-  })
-);
-
-self.addEventListener('fetch', event => {
-  if (event.request.method == 'POST') {
-  event.respondWith((async () => {
-    const formData = await event.request.formData();
-    const link = formData.get('link') || '';
-    const responseUrl = await saveBookmark(link);
-    return Response.redirect(responseUrl, 303);
-  })());
-}
-});
 
 self.addEventListener('fetch', async function(event) {
 if (!navigator.onLine) {
