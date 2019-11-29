930672dccdbab76ad59c8c781b994d6b051d5065
abhishek <abhishek7gg7@gmail.com>
2019-11-29 19:09:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
-  /.*/,
+  /\.(?:html|js)$/,
   new workbox.strategies.StaleWhileRevalidate(),
 );
 
