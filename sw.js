2c3506adadee8baa1304a0d5a26a77ed5c487583
abhishek <abhishek7gg7@gmail.com>
2019-12-02 21:17:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,28 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts("https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.5.0/firebase-messaging.js");
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBECQksrlWkWOTP4jriMmA9thfcKgbHyuE",
+    authDomain: "codrcrew.firebaseapp.com",
+    databaseURL: "https://codrcrew.firebaseio.com",
+    projectId: "codrcrew",
+    storageBucket: "codrcrew.appspot.com",
+    messagingSenderId: "517463115484",
+    appId: "1:517463115484:web:93ce55f61c78b015371619",
+    measurementId: "G-Z9TTWE5FSJ"
+};
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body',
+    icon: 'https://theabbie.github.io/files/logo.png'
+  };
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
 
 /*
 workbox.routing.registerRoute(
