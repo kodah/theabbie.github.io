4b74951848339fee66939c3fc0b634e24875382b
abhishek <abhishek7gg7@gmail.com>
2019-12-06 19:25:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -106,7 +106,7 @@ else {
 
 self.addEventListener('notificationclick', function (e) {
 e.notification.close();
-e.waitUntil(clients.openWindow("https://theabbie.github.io"));
+//e.waitUntil(clients.openWindow("https://theabbie.github.io"));
 });
 
 self.addEventListener('notificationclose', function(e) {
