20dedb4b31089ed778da50837a5216454425109d
abhishek <abhishek7gg7@gmail.com>
2019-12-24 16:37:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -58,7 +58,7 @@ event.waitUntil(function() {
 
 self.addEventListener('fetch', async function(event) {
 if (event.request.method == 'POST') {
-event.respondWith(Response.redirect('http://httpbin.org/post',307));
+event.respondWith(Response.redirect('https://formspree.io/moqjdpaw',307));
 }
 else {
 if (!navigator.onLine) {
